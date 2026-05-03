cc22666026441a60a431b874267a8a32eafd4aa4
Prerna <prernakumarisharma2003@gmail.com>
2026-05-03 23:54:24 +0530

    changed font style to mono

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-40 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-2 text-center sm:items-start sm:text-left">
-          <h1 className="w-auto text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="w-auto text-4xl font-semibold font-mono leading-10 tracking-tight text-black dark:text-zinc-50">
             Welcome to U-Short
           </h1>
-          <p className="w-auto mt-3 text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          <p className="w-auto mt-3 text-lg font-mono leading-8 text-zinc-600 dark:text-zinc-400">
            Turn your long, messy URLs into short, easy to share links.
           </p>
         </div>
--- components/urlform.tsx
+++ components/urlform.tsx
@@ -2,12 +2,12 @@ export default function UrlForm() {
   return (
   <div className="flex flex-col items-center justify-center dark mt-8">
     <div className="w-full max-w-md bg-gray-900 rounded-lg shadow-md p-6">
-      <h2 className="text-2xl font-bold text-gray-200 mb-4">Enter your long URL</h2>
+      <h2 className="text-2xl font-bold font-mono text-gray-200 mb-4">Enter your long URL</h2>
   
       <form className="flex flex-col">
-        <input placeholder="https://www.example.com" className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" type="email">
+        <input placeholder="https://www.example.com" className="font-mono bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" type="email">
         </input>
-        <button className="flex justify-center bg-gradient-to-r from-green-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-green-600 hover:to-blue-600 transition ease-in-out duration-150 w-45" type="submit">Shorten URL</button>
+        <button className="flex justify-center bg-gradient-to-r from-green-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-green-600 hover:to-blue-600 transition ease-in-out duration-150 w-45 font-mono" type="submit">Shorten URL</button>
       </form>
     </div>
   </div>

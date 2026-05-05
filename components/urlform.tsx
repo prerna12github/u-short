4263a02bb45d74ef50da6b3efa8ad17e17d8cce7
Prerna <prernakumarisharma2003@gmail.com>
2026-05-05 14:10:17 +0530

    fix error message

--- components/urlform.tsx
+++ components/urlform.tsx
@@ -5,29 +5,35 @@ export default function UrlForm() {
   const [url, setUrl] = useState('');
   const [shortUrl, setShortUrl] = useState('');
   const [status, setStatus] = useState('');
+  const [errors, setErrors] = useState('');
   
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus('loading');    
-    try {
-      const response = await fetch(`http://localhost:8000/shorten?url=${encodeURIComponent(url)}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ url }),
-      });
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      setShortUrl(data.shortUrl);
-      setStatus('success');
-    } catch (error) {
-      console.error(error);
-      setStatus('error');
-    }
-  };
+     e.preventDefault();
+     setStatus('loading');    
+     try {
+       const response = await fetch(`http://localhost:8000/shorten?url=${encodeURIComponent(url)}`, {
+         method: 'POST',
+         headers: {
+           'Content-Type': 'application/json',
+         },
+         body: JSON.stringify({ url }),
+       });
+       const data = await response.json();
+       console.log(data)
+       if (!response.ok) {
+         setErrors(data.detail);
+         setStatus('error');
+       } else {
+         setShortUrl(data.shortUrl);
+         setStatus('success');
+       }
+     } catch (error) {
+       console.error(error);
+       setStatus('error');
+       setErrors(error.message);
+     }
+   };
+
   return (
     <div className="flex flex-col items-center justify-center dark mt-8">
       <div className="w-full max-w-md bg-gray-900 rounded-lg shadow-md p-6">
@@ -38,7 +44,7 @@ export default function UrlForm() {
           </input>
           <button className="flex justify-center bg-gradient-to-r from-green-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-green-600 hover:to-blue-600 transition ease-in-out duration-150 w-45 font-mono" type="submit" onClick={handleSubmit}>Shorten URL</button>
           {status === 'loading' && <p className="font-mono text-gray-200 mt-4">Shortening...</p>}
-          {status === 'error' && <p className="font-mono text-red-500 mt-4">Error: {status} </p>}
+          {status === 'error' && <p className="font-mono text-red-500 mt-4">Error: {errors}</p>}
           {shortUrl && <p className="font-mono text-gray-200 mt-4">Your short URL: {shortUrl} </p>}
         </form>
       </div>

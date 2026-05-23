f8fb254557d023fb0f125df543ba7549e44f5a57
Prerna <prernakumarisharma2003@gmail.com>
2026-05-23 19:52:30 +0530

    changed next-js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "http://localhost:8000",
+        "https://u-shortner.vercel.app",
+      ],
+    },
+  },
 };
 
 export default nextConfig;

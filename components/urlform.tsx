f68db5534f68facb73b1e4101cd88d6e78433f5e
Prerna <prernakumarisharma2003@gmail.com>
2026-05-31 13:35:11 +0530

    fixxieee

--- components/urlform.tsx
+++ components/urlform.tsx
@@ -47,71 +47,66 @@ export default function UrlForm() {
     console.log(shorturl);
     const code = shorturl.split('/').pop();
     console.log(code);
-    const redirect = await fetch(`https://u-shortner.vercel.app/${code}`, { method: "GET" });
-    const res = await redirect.json();
-    console.log(res);
-  if (redirect.ok) {
-    const location = redirect.headers.get("location");
-    console.log(location);
-    if (location) {
-      window.location.href = location;
-    }
-  }
-};
+    await fetch(`https://u-shortner.vercel.app/${code}`,
+      {
+        method: "GET"
+      }
+    );
+  };
 
-  return (
-    <div className="flex flex-col items-center justify-center dark mt-8">
-      <div className="w-full max-w-md bg-gray-900 rounded-lg shadow-md p-6">
-        <h2 className="text-2xl font-bold font-mono text-gray-200 mb-4">
-          Enter your long URL
-        </h2>
+    return (
+      <div className="flex flex-col items-center justify-center dark mt-8">
+        <div className="w-full max-w-md bg-gray-900 rounded-lg shadow-md p-6">
+          <h2 className="text-2xl font-bold font-mono text-gray-200 mb-4">
+            Enter your long URL
+          </h2>
 
-        <form className="flex flex-col">
-          <input
-            placeholder="https://www.example.com"
-            className="font-mono bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
-            type="url"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-          ></input>
-          <button
-            className="flex justify-center bg-linear-to-r from-green-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-green-600 hover:to-blue-600 transition ease-in-out duration-150 w-45 font-mono"
-            type="submit"
-            onClick={handleSubmit}
-          >
-            Shorten URL
-          </button>
-          {status === "loading" && (
-            <p className="font-mono text-gray-200 mt-4">Shortening...</p>
-          )}
-          {status === "error" && (
-            <p className="font-mono text-red-500 mt-4">Error: {errors}</p>
-          )}
-          {shortUrl && (
-            <p className="font-mono text-gray-200 mt-4" onClick={() => redirectUrl(shortUrl)}>
-              Your short URL : <a href={shortUrl} target="_blank" rel="noopener noreferrer">{shortUrl}</a>
-            </p>
-          )}
-          
-          <div className="flex gap-8">
-          {shortUrl && (
+          <form className="flex flex-col">
+            <input
+              placeholder="https://www.example.com"
+              className="font-mono bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+              type="url"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+            ></input>
             <button
-              className="flex justify-center bg-linear-to-r from-blue-500 to-green-500 text-white font-bold py-2 px-4 rounded-md mt-4  hover:to-blue-600 transition ease-in-out duration-150 w-45 font-mono"
-              type="button"
-              onClick={() => {
-                navigator.clipboard.writeText(shortUrl);
-                setVisible(true);
-              }}
+              className="flex justify-center bg-linear-to-r from-green-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-green-600 hover:to-blue-600 transition ease-in-out duration-150 w-45 font-mono"
+              type="submit"
+              onClick={handleSubmit}
             >
-              Copy
+              Shorten URL
             </button>
-          )}
-          </div>
-          {visible && <AlertBasic />}
-          {visible && <button className="flex justify-center bg-linear-to-r from-red-500 to-red-600 text-white font-bold py-2 px-4 rounded-md mt-4  hover:to-red-700 transition ease-in-out duration-150 w-20 font-mono" onClick={() => setVisible(false)}>Close</button>}
-        </form>
-      </div>
+            {status === "loading" && (
+              <p className="font-mono text-gray-200 mt-4">Shortening...</p>
+            )}
+            {status === "error" && (
+              <p className="font-mono text-red-500 mt-4">Error: {errors}</p>
+            )}
+            {shortUrl && (
+              <p className="font-mono text-gray-200 mt-4" onClick={() => redirectUrl(shortUrl)}>
+                Your short URL : <a href={shortUrl} target="_blank" rel="noopener noreferrer">{shortUrl}</a>
+              </p>
+            )}
+          
+            <div className="flex gap-8">
+              {shortUrl && (
+                <button
+                  className="flex justify-center bg-linear-to-r from-blue-500 to-green-500 text-white font-bold py-2 px-4 rounded-md mt-4  hover:to-blue-600 transition ease-in-out duration-150 w-45 font-mono"
+                  type="button"
+                  onClick={() => {
+                    navigator.clipboard.writeText(shortUrl);
+                    setVisible(true);
+                  }}
+                >
+                  Copy
+                </button>
+              )}
+            </div>
+            {visible && <AlertBasic />}
+            {visible && <button className="flex justify-center bg-linear-to-r from-red-500 to-red-600 text-white font-bold py-2 px-4 rounded-md mt-4  hover:to-red-700 transition ease-in-out duration-150 w-20 font-mono" onClick={() => setVisible(false)}>Close</button>}
+          </form>
+        </div>
     
-     </div>
-  );
-}
+      </div>
+    );
+  };
